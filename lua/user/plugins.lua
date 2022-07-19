acdee5edb548e630b4866180241049c49d05c0aa
Pascal Hubrecht <sarl@hubrecht.info>
2022-07-19 21:44:47 +0200

    vim surround

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -85,7 +85,10 @@ return packer.startup(function(use)
   use { "L3MON4D3/LuaSnip", commit = "79b2019c68a2ff5ae4d732d50746c901dd45603a" } --snippet engine
   use { "rafamadriz/friendly-snippets", commit = "d27a83a363e61009278b6598703a763ce9c8e617" } -- a bunch of snippets to use
 
-  use "hansemschnokeloch/vim-twig"
+  use { "hansemschnokeloch/vim-twig" }
+
+  -- Tim Pope
+  use { "tpope/vim-surround", commit = "bf3480dc9ae7bea34c78fbba4c65b4548b5b1fea" }
 
   -- LSP
   use { "neovim/nvim-lspconfig", commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" } -- enable LSP
